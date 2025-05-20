fa4e42fe48ecdd0c6eeeff84e7cc8fbb73b9a626
MigueUrrutia <163359725+MigueUrrutia@users.noreply.github.com>
2025-05-20 11:02:44 -0600

    correcion de errores

--- frontend/PeaceNest/utils/storage.js
+++ frontend/PeaceNest/utils/storage.js
@@ -1,14 +1,28 @@
-
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export async function saveFavoriteQuote(quote) {
   const stored = await AsyncStorage.getItem('favorites');
   const list = stored ? JSON.parse(stored) : [];
-  list.push(quote);
+
+  // Estandarizamos el formato: { q: "...", a: "..." }
+  const nuevo = {
+    q: quote.q || quote.texto || '',
+    a: quote.a || quote.autor || ''
+  };
+
+  list.push(nuevo);
   await AsyncStorage.setItem('favorites', JSON.stringify(list));
 }
 
 export async function getFavorites() {
   const stored = await AsyncStorage.getItem('favorites');
   return stored ? JSON.parse(stored) : [];
+}
+export async function removeFavorite(indexToRemove) {
+  const stored = await AsyncStorage.getItem('favorites');
+  const list = stored ? JSON.parse(stored) : [];
+
+  const newList = list.filter((_, index) => index !== indexToRemove);
+  await AsyncStorage.setItem('favorites', JSON.stringify(newList));
+  return newList;
 }
